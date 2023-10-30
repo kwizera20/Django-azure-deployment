6d9abd9761937c2cf56e189d25473a483b7ed517
= <=>
2023-10-30 20:31:40 +0200

    modify the settings for deployment file

--- greatkart/deployment.py
+++ greatkart/deployment.py
@@ -2,9 +2,9 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-SECRET_KEY = 'ijUALkruME'
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
+SECRET_KEY = os.environ.get('SECRET', 'default_secret_key')
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', 'example.com')]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('WEBSITE_HOSTNAME', 'example.com')]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -17,19 +17,18 @@ MIDDLEWARE = [
     'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.compressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
+connection_string = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING', '')
+parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split()}
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': parameters['dbname'],
-            'USER': parameters['user'],
-            'PASSWORD': parameters['password'],
-            'HOST': parameters['host'],
-           
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parameters.get('dbname', 'default_dbname'),
+        'USER': parameters.get('user', 'default_user'),
+        'PASSWORD': parameters.get('password', 'default_password'),
+        'HOST': parameters.get('host', 'localhost'),
     }
+}
