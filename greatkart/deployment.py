93f865601c77039d1a78215bfb818e6d03636472
= <=>
2023-10-30 21:03:14 +0200

    modify the  deployment file

--- greatkart/deployment.py
+++ greatkart/deployment.py
@@ -2,13 +2,15 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-SECRET_KEY = os.environ.get('SECRET', 'default_secret_key')
-ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', 'example.com')]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('WEBSITE_HOSTNAME', 'example.com')]
+SECRET_KEY = os.environ['SECRET']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
+DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -20,15 +22,16 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING', '')
-parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split()}
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': parameters.get('dbname', 'default_dbname'),
-        'USER': parameters.get('user', 'default_user'),
-        'PASSWORD': parameters.get('password', 'default_password'),
-        'HOST': parameters.get('host', 'localhost'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': parameters['dbname'],
+            'USER': parameters['user'],
+            'PASSWORD': parameters['password'],
+            'HOST': parameters['host'],
+           
+        }
     }
-}
