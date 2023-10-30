386c48d34869050057c346cba1e6fe0428ed97de
= <=>
2023-10-30 11:30:25 +0200

    first commit

---
+++ greatkart/deployment.py
@@ -0,0 +1,34 @@
+import os
+from .settings import *
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = [os.environ['WEDSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.compressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+connection_string = os.environ['AZURE_POSTRESQL_CONNECTIONSTRING']
+parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': parameters['dbname'],
+            'USER': parameters['user'],
+            'PASSWORD': parameters['password'],
+            'HOST': parameters['host'],
+           
+        }
+    }
