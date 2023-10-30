2183080e44251790a8c2318062644c9aacb5fe1f
= <=>
2023-10-30 20:25:35 +0200

    modify the settings for deployment

--- greatkart/deployment.py
+++ greatkart/deployment.py
@@ -2,8 +2,8 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEDSITE_HOSTNAME']]
+SECRET_KEY = 'ijUALkruME'
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 
 MIDDLEWARE = [
@@ -20,7 +20,7 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.compressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ['AZURE_POSTRESQL_CONNECTIONSTRING']
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
 
 DATABASES = {
