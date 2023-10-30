79d2d4aeae2ea4f75846827955ff3420fee4cdd3
= <=>
2023-10-30 17:37:56 +0200

    modify the settings for env

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-SECRET_KEY = os.environ['SECRET']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
